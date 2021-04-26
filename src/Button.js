33f43189f0e29c64e7d5f5fa6acbfeed49d1ebba
narwhalblast <simonsc.chen@mail.utoronto.ca>
2021-04-26 19:46:00 -0400

    Firebase + ReactApp

---
+++ src/Button.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import unpressed from './button-unpressed.png';
+import pressed from './button-unpressed.png';
+
+const Button = () => {
+    return (
+        <button>
+            <img src="./button-unpressed.png"></img>
+        </button>
+    );
+};
+
+export default Button;
